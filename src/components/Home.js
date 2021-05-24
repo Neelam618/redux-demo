c3951ede304e3ed2dbd46de65fce922346bb639a
Neelam618 <neelamchavan410@gmail.com>
2021-05-24 12:38:26 +0530

    add to cart design done

---
+++ src/components/Home.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div>
+            <div className="add-to-cart">
+                <img src="https://previews.123rf.com/images/wiseant/wiseant2010/wiseant201000031/156610029-cart-icon-buy-in-online-shop-basket-for-grocery-symbol-of-store-button-of-trolley-for-add-purchase-i.jpg" alt="" />
+            </div>
+            <h1>home component </h1>
+            <div className="cart-wrapper">
+                <div className="img-wrapper item">
+                    <img src="https://wmstatic.global.ssl.fastly.net/ml/4101116-4147319.png?width=460&height=800" alt="" />
+                </div>
+                <div className="text-wrapper item">
+                    <span>
+                        Iphone
+                    </span>
+                    <span>
+                        Price: $1000.00
+                    </span>
+                </div>
+                <div className="btn-wrapper item">
+                    <button>Add to cart</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
